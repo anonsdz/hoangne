400121eb35458e76760ec126beef922ba4d174d3
anonsdz <61068030+anonsdz@users.noreply.github.com>
2024-12-11 04:57:17 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP App</title>
+</head>
+<body>
+    <h1>Welcome to My PHP App!</h1>
+    <p>This is accessible by anyone with the correct IP:Port.</p>
+</body>
+</html>
